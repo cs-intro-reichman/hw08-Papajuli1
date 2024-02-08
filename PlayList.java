7e4c00e148e640770c87d8b84a7547f459d51ced
Papajuli1 <yonatan.abramovich@post.runi.ac.il>
2024-02-08 16:25:22 +0200

    El Final

--- PlayList.java
+++ PlayList.java
@@ -80,9 +80,14 @@ class PlayList {
 
     /** Returns the total duration (in seconds) of all the tracks in this list. */
     public int totalDuration() {
+        // int total = 0;
+        // for (int i = 0; i < size; i++) {
+        // total += getTrack(i).getDuration();
+        // }
+        // return total;
         int total = 0;
-        for (int i = 0; i < size; i++) {
-            total += getTrack(i).getDuration();
+        for (int i = 0; i < this.size; i++) {
+            total += this.getTrack(i).getDuration();
         }
         return total;
     }
@@ -92,9 +97,16 @@ class PlayList {
      * If such a track is not found, returns -1.
      */
     public int indexOf(String title) {
-        for (int i = 0; i < size; i++) {
-            if (getTrack(i) != null) {
-                if (getTrack(i).getTitle().toLowerCase().equals(title.toLowerCase()))
+        // for (int i = 0; i < size; i++) {
+        // if (getTrack(i) != null) {
+        // if (getTrack(i).getTitle().toLowerCase().equals(title.toLowerCase()))
+        // return i;
+        // }
+        // }
+        // return -1;
+        for (int i = 0; i < this.size; i++) {
+            if (this.getTrack(i) != null) {
+                if (this.getTrack(i).getTitle().toLowerCase().equals(title.toLowerCase()))
                     return i;
             }
         }
@@ -110,15 +122,28 @@ class PlayList {
      * returns true.
      */
     public boolean add(int i, Track track) {
-        if (i >= 0 && i <= size && size < maxSize) {
-            if (i == size)
-                add(track);
+        // if (i >= 0 && i <= size && size < maxSize) {
+        // if (i == size)
+        // add(track);
+        // else {
+        // for (int j = size; j > i; j--) {
+        // tracks[j] = tracks[j - 1];
+        // }
+        // tracks[i] = track;
+        // size++;
+        // }
+        // return true;
+        // }
+        // return false;
+        if (i >= 0 && i <= this.size && this.size < this.maxSize) {
+            if (i == this.size)
+                this.add(track);
             else {
-                for (int j = size; j > i; j--) {
-                    tracks[j] = tracks[j - 1];
+                for (int j = this.getSize(); j > i; j--) {
+                    this.tracks[j] = this.tracks[j - 1];
                 }
-                tracks[i] = track;
-                size++;
+                this.tracks[i] = track;
+                this.size = size + 1;
             }
             return true;
         }
@@ -132,14 +157,25 @@ class PlayList {
      * does nothing and returns -1.
      */
     public void remove(int i) {
-        if (size != 0 && i >= 0 && i < size) {
-            if (i == size - 1)
-                removeLast();
+        // if (size != 0 && i >= 0 && i < size) {
+        // if (i == size - 1)
+        // removeLast();
+        // else {
+        // for (int j = i; j < size; j++) {
+        // tracks[j] = tracks[j + 1];
+        // }
+        // size--;
+        // }
+
+        // }
+        if (this.getSize() != 0 && i >= 0 && i < this.getSize()) {
+            if (i == this.size - 1)
+                this.removeLast();
             else {
-                for (int j = i; j < size; j++) {
-                    tracks[j] = tracks[j + 1];
+                for (int j = i; j < this.getSize(); j++) {
+                    this.tracks[j] = this.tracks[j + 1];
                 }
-                size--;
+                this.size = size - 1;
             }
 
         }
@@ -151,9 +187,12 @@ class PlayList {
      * is negative or too big for this list, does nothing.
      */
     public void remove(String title) {
-        int index = indexOf(title);
-        if (index != -1 && size != 0 && index >= 0 && index < size)
-            remove(index);
+        // int index = indexOf(title);
+        // if (index != -1 && size != 0 && index >= 0 && index < size)
+        // remove(index);
+        int index = this.indexOf(title);
+        if (index != -1 && this.getSize() != 0 && index >= 0 && index < this.getSize())
+            this.remove(index);
     }
 
     /**
@@ -162,6 +201,8 @@ class PlayList {
     public void removeFirst() {
         if (size > 0)
             remove(0);
+        // if (this.getSize() > 0)
+        // this.remove(0);
     }
 
     /**
